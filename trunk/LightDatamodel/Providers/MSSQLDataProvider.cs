2c1bd74fdbda92cace54d877a515d2b69a4d87b4
morten@kvistgaard.org <morten@kvistgaard.org@46887226-b942-0410-9c47-cf8052e9876e>
2008-08-25 09:34:20 +0000

    null check

--- trunk/LightDatamodel/Providers/MSSQLDataProvider.cs
+++ trunk/LightDatamodel/Providers/MSSQLDataProvider.cs
@@ -277,7 +277,7 @@ namespace System.Data.LightDatamodel
 			if (value == null || value == DBNull.Value) return "NULL";
             
             IDataParameter p = cmd.CreateParameter();
-			if (value.GetType() == typeof(string) && (string)value == "")
+			if (value == null || (value.GetType() == typeof(string) && (string)value == ""))
 				p.Value = DBNull.Value;
 			else if (value.GetType() == typeof(DateTime))
 			{
