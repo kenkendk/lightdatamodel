b69b389fec7df6b77b3076f2445c1bd6c8d2dba1
morten@kvistgaard.org <morten@kvistgaard.org@46887226-b942-0410-9c47-cf8052e9876e>
2008-11-06 13:47:26 +0000

    Meh

---
+++ trunk/Unit test/Datamodel/TestView.cs
@@ -0,0 +1,18 @@
+using System.Data.LightDatamodel;
+using System.Data.LightDatamodel.DataClassAttributes;
+
+namespace Datamodel.UnitTest
+{
+	[DatabaseView("SELECT ID FROM Project")]
+	public class TestView : DataClassView
+	{
+		[DatabaseField("ID")]
+		private System.Int64 m_ID = 0;
+
+		public System.Int64 ID
+		{
+			get { return m_ID; }
+			set { m_ID = value; }
+		}
+	}
+}
